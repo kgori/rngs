b5c65829808bbe3d05755ff2083d609b6a1f2129
Kevin Gori <kcg25@cam.ac.uk>
2023-11-22 11:53:22 +0000

    Added a couple of comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 #include "lib.h"
 
+/* This doesn't do anything except make the
+*  compiler confirm that the T is a valid RNG. */
 template<typename T>
 requires std::uniform_random_bit_generator<T>
-void check_rng(T rng) {
+void check_rng(T rng) {}
 
-}
-
+/* What else would you do with a random number generator? 
+*  Generate a bunch of random numbers. */
 int main(int argc, char ** argv) {
-    std::cout << "rnd" << std::endl;
     xorshift32 rng{1010203};
     check_rng(rng);
     
